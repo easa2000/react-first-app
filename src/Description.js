879efedf8326634007f4f6e32148e37a13a823e4
Mahudoom Naina <66937573+easa2000@users.noreply.github.com>
2023-02-20 12:33:55 +0530

    Add files via upload

---
+++ src/Description.js
@@ -0,0 +1,14 @@
+function Description(){
+    return(
+        <html>
+
+        
+        <h1>DESCRIPTION</h1>
+
+        <p>This is a Kentucky Fried Chicken family bucket used in KFC restaurants around 1969. The bucket bears the trademark image of Harland Sanders in his Colonel Sanders outfit—white suit and black string tie—as well as the copyrighted tagline “it's finger lickin' good.” The KFC Chicken 
+            bucket was first created in 1957 by Sanders and his first franchisee, Pete Harman. It contained 15 pieces of chicken, a pint of gravy, and biscuits. The idea was sold as a way for housewives to escape the kitchen dinner and still prepare a great dinner, merely needing to add a salad and a vegetable for a balanced meal.</p>
+        </html>
+    )
+}
+
+export default Description;
---
+++ src/Price.js
@@ -0,0 +1,11 @@
+function Price(){
+    return(
+        <html>
+            <h1>PRICE</h1>
+            <h4>&#8377;646.00</h4>
+            <p>Save 33% on 4pc Hot & Crispy, 6 Wings, 4 Strips, 2 Dips (20gm each) & 1 Pepsi 500ml</p>
+        </html>
+    )
+}
+
+export default Price;
